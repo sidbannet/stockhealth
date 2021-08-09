427da3c35e6c2b332d799bdb72c0fe562e304ca3
Siddhartha Banerjee <sidban@uwalumni.com>
2021-08-08 23:35:39 -0700

    Adding trends
    Trends technical analysis on a stock.

--- stockhealth/analyzer.py
+++ stockhealth/analyzer.py
@@ -7,6 +7,7 @@
 # Copyright 2021 [Siddhartha Banerjee](mailto:sidban@uwalumni.com)
 #
 
+import matplotlib.pyplot as plt
 import yfinance as yf
 
 
@@ -19,6 +20,7 @@ class TimeSeries:
     ):
         """Instantiate the class."""
         self.__ticker = yf.Ticker(ticker)
+        self.__symbol = ticker
         self.__history = self.__ticker.history(period='max', interval='1d')
         df = self.__history
         df['Increase'] = df['Close'].diff(periods=1) / df['Open'] * 100
@@ -27,3 +29,31 @@ class TimeSeries:
             (df['Close'].diff(periods=1) - df['Dividends']).cumsum()
         df['Increase in Real Worth'] = df['Real Worth'].diff(periods=1) / \
             df['Real Worth'].shift(periods=1) * 100
+
+    def trends(
+            self,
+            window: int = int(10),
+    ) -> tuple:
+        """Get the technical analysis trends."""
+        fig = plt.figure('Trends analysis: ' + self.__symbol)
+        axs = fig.subplots(nrows=1, ncols=1)
+        df = self.__history
+        df['Real Worth'].rolling(
+            window=window, closed='right'
+        ).mean().plot(
+            style='-.', color='k', label='mean', ax=axs,
+        )
+        df['Real Worth'].rolling(
+            window=window, closed='right',
+        ).max().plot(style='--', color='r', label='max', ax=axs,)
+        df['Real Worth'].rolling(
+            window=window, closed='right',
+        ).min().plot(style='--', color='b', label='min', ax=axs,)
+        df['Real Worth'].plot(
+            style='.', color='k', label='Worth', ax=axs,
+            title='History', grid=True,
+        )
+        axs.legend()
+        axs.set_ylabel('Real Worth [USD]')
+        fig.suptitle('Trends analysis')
+        return fig, axs

e0d38e0f7fc84ecf84a56aa3067857083b68407c
Siddhartha Banerjee <sidban@uwalumni.com>
2021-08-28 16:40:13 -0700

    Update simulation
    Now compatible with the model side updates on multiple instances of simulation at the same time.

--- stockhealth/simulation.py
+++ stockhealth/simulation.py
@@ -27,24 +27,42 @@ class MonteCarlo:
         self.t_end = time_end
         self.dt = dt
         self.__solved = False
-        self.solution = pd.DataFrame(data=None)
+        self.S = pd.DataFrame(
+            data=None,
+            index=[],
+            columns=list(range(model.S.size)),
+        )
+        self.V = pd.DataFrame(
+            data=None,
+            index=[],
+            columns=list(range(model.volatility.size)),
+        )
+        self.__name_dataframe_index(name='t')
         # //todo: assert if the simulation is setup correctly
 
     def solve(self) -> None:
         """Solver for the simulation."""
-        t = 0
-        time = [t, ]
-        S = [self._mdl.S, ]
-        while t <= self.t_end:
-            t += self.dt
+        S = self.S.copy()
+        V = self.V.copy()
+        while self._mdl.t <= self.t_end:
             self._mdl.update(dt=self.dt)
-            time.append(t)
-            S.append(self._mdl.S)
-        self.solution = pd.DataFrame(
-            {
-                't': time,
-                'S': S,
-            }
-        ).set_index('t')
+            S = S.append(
+                pd.DataFrame(
+                    data=self._mdl.S, columns=[self._mdl.t]
+                ).T,
+            )
+            V = V.append(
+                pd.DataFrame(
+                    data=self._mdl.volatility, columns=[self._mdl.t]
+                ).T,
+            )
+        self.S = S.copy()
+        self.V = V.copy()
+        self.__name_dataframe_index(name='t')
+        self._mdl.reset()
         self.__solved = True
         # //todo: reset the model after the solver
+
+    def __name_dataframe_index(self, name: str = 't') -> None:
+        """Name the dataframe index column."""
+        self.S.index.name = self.V.index.name = name

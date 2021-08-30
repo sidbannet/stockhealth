479d3b37b6fe24a3f3936d69be8f80d14c0e6b08
Siddhartha Banerjee <sidban@uwalumni.com>
2021-08-29 21:01:59 -0700

    Update simulation
    Got market schedule and updated the simulation solution with the market schedule. Additional simulation option given for day trading (not a calendar date event updates for simulation results).

--- stockhealth/simulation.py
+++ stockhealth/simulation.py
@@ -9,6 +9,8 @@
 
 import numpy as np
 import pandas as pd
+from pandas_market_calendars import get_calendar as market_calendar
+from datetime import datetime, timedelta
 from stockhealth.model import StochasticVolatility as Model
 
 
@@ -19,14 +21,20 @@ class MonteCarlo:
     def __init__(
             self,
             model: Model = None,
-            time_end: np.float = np.nan,
-            dt: np.float = np.float(1 / 365),
+            number_of_days: np.int = np.nan,
+            steps_in_days: np.int = np.int(1),
+            stock_exchange_name: str = 'NYSE',
+            start_date: datetime = datetime.today().date(),
     ):
         """Setup the simulation environment."""
         self._mdl = model
-        self.t_end = time_end
-        self.dt = dt
+        self.t_end = np.float(number_of_days / 365)
+        self.dt = np.float(steps_in_days / 365)
+        self.__steps_in_days = steps_in_days
         self.__solved = False
+        self.__start_date = start_date
+        self.__number_of_days = number_of_days
+        self.__exchange_calendar = market_calendar(stock_exchange_name)
         self.S = pd.DataFrame(
             data=self._mdl.S, columns=[self._mdl.t],
         ).T
@@ -36,10 +44,10 @@ class MonteCarlo:
         self.__name_dataframe_index(name='t')
         # //todo: assert if the simulation is setup correctly
 
-    def solve(self) -> None:
+    def solve(self, day_trading: bool = False) -> None:
         """Solver for the simulation."""
-        S = self.S.copy()
-        V = self.V.copy()
+        assert not self.__solved, "This simulation is already done!!!"
+        S, V = self.S.copy(), self.V.copy()
         while self._mdl.t <= self.t_end:
             self._mdl.update(dt=self.dt)
             S = S.append(
@@ -52,12 +60,20 @@ class MonteCarlo:
                     data=self._mdl.volatility, columns=[self._mdl.t]
                 ).T,
             )
-        self.S = S.copy()
-        self.V = V.copy()
-        self.__name_dataframe_index(name='t')
+        self.S, self.V = S.copy(), V.copy()
+        if not day_trading:
+            scheduled_days = self.__exchange_calendar.schedule(
+                start_date=self.__start_date,
+                end_date=self.__start_date + timedelta(
+                    days=2 * len(self.S) + 50),
+            )
+            dates = scheduled_days.index[:self.S.__len__()].date
+            self.S.index = self.V.index = dates
+            self.__name_dataframe_index(name='Date')
+        else:
+            self.__name_dataframe_index(name='t')
         self._mdl.reset()
         self.__solved = True
-        # //todo: reset the model after the solver
 
     def __name_dataframe_index(self, name: str = 't') -> None:
         """Name the dataframe index column."""

872ce59c779ef80fd79f4458e1168a137aedfd3a
Siddhartha Banerjee <sidban@uwalumni.com>
2021-09-16 20:22:11 -0700

    Create `training` module
    The module for training model parameter based on historical patterns is created.

---
+++ stockhealth/training.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# StockHealth simulator
+# [StockHealth GitRepo](https://github.com/sidbannet/stockhealth)
+#
+# Copyright 2021 [Siddhartha Banerjee](mailto:sidban@uwalumni.com)
+#

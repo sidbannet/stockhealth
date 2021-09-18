06fc302a8314216b2d8de8e2ddba09dee83c68bf
Siddhartha Banerjee <sidban@uwalumni.com>
2021-09-18 10:50:48 -0700

    Update analyzer
    Prepare for the risk-free returns of stock price. Fix the definitions and take the return in unit-less numbers instead of [%].

--- stockhealth/analyzer.py
+++ stockhealth/analyzer.py
@@ -25,12 +25,16 @@ class TimeSeries:
         self.__history = self.__ticker.history(period='max', interval='1d')
         df = self.__history
         df['Increase [%]'] = df['Close'].diff(periods=1) / df['Open'] * 100
-        df['Volatility'] = (df['High'] - df['Low']) / df['Open'] * 100
+        df['Intra-day volatility'] = (
+            df['High'] - df['Low']
+        ) / (
+            df['Close'] - df['Open']
+        )
         df['Real Worth'] = df['Close'][0] + \
             (df['Close'].diff(periods=1) - df['Dividends']).cumsum()
         df['Real Worth'].values[0] = df['Close'].values[0]
-        df['Increase in Real Worth [%]'] = df['Real Worth'].diff(periods=1) / \
-            df['Real Worth'].shift(periods=1) * 100
+        df['Risk free return'] = df['Real Worth'].diff(periods=1) / \
+            df['Real Worth'].shift(periods=1)
 
     def technical(
             self,

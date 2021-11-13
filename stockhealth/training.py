3cd9fad7b3c3dbe3ec2ed53b614603607deaca9d
Siddhartha Banerjee <sidban@uwalumni.com>
2021-11-12 22:05:19 -0800

    Closes #26 (#27)
    * Few more volatility parameters
    
    Intra-day volatility and volatility is added to analyzer.
    
    * WIP to extract Heston Model features
    
    WIP on extracting Heston Model features for an underlying stock price based on stochastic volatility model.
    
    * Minor liner issue
    
    Issue with code indentation fixed.
    
    * WIP Update training
    
    Bug in volatility formulation corrected.
    
    * Fix typo
    
    Spell error fixed.
    
    * Create an example to extract volatility
    
    Trying to get an example notebook going.
    
    * Example python notebook to do technical analysis of stock.
    
    * Update .gitignore
    
    Let virtual environment be added with the repository.
    
    * Update .gitignore
    
    Let `vene` stay with the repo
    
    * Commiting virtual environment
    
    * Few example notebooks are added
    
    * Example notebook for options prices.
    
    * Staging WIP analysis for feature extraction of stock
    
    * Rename virtual environment
    
    Making virtual environment hidden
    
    * Hidden virtual environment
    
    Hidden virtual environment .venv now included with the repo.
    
    * WIP Extract Heston model features using KDE
    
    * Refactoring training to incorporate number of days
    
    * Get KDE returned as feature extract
    
    * Cleaning up the code for KDE extraction
    
    * Fix return type for KDE
    
    * WIP analysis of mean reversion rate
    
    * Trying to find the mean and std of mean reversion
    
    * Update training
    
    PEP8 warning fixed.
    
    * Update training
    
    Changing the trends extraction for volatility based on ratio of current volatility and the historical volatility.
    
    * Impliment hubber regression to model training
    
    * Correction with training
    
    Training model for stochastic volatility corrected to get the log normal regression.
    
    * Update training
    
    Include standard deviation for the mean and volatility

--- stockhealth/training.py
+++ stockhealth/training.py
@@ -9,7 +9,11 @@
 
 import numpy as np
 from stockhealth.analyzer import TimeSeries as Stock
+from scipy.stats import gaussian_kde
+from sklearn.linear_model import HuberRegressor as Regressor
 from stockhealth.model import _NUMBER_OF_TRADING_DAYS_PER_YEAR as _NTD
+
+vectorized_log = np.vectorize(np.log)
 
 
 class Trends:
@@ -18,7 +22,7 @@ class Trends:
     def __init__(
             self,
             stock: Stock = None,
-            number_of_days: np.int = np.nan,
+            number_of_days: np.int = int(1),
     ):
         """Instantiate the class."""
         self.stock = stock
@@ -26,8 +30,10 @@ class Trends:
         df = stock.history__
         if number_of_days is not np.nan and number_of_days >= _NTD:
             data = df['Risk free return'][-number_of_days:]
+            self.number_of_days = number_of_days
         else:
             data = df['Risk free return']
+            self.number_of_days = int(30)
         self.__std = data.std() * np.sqrt(_NTD)
         self.__mew = data.mean() * _NTD
         self.__S = df['Close'][-1]
@@ -40,3 +46,72 @@ class Trends:
             'std': self.__std,
             'latest close': self.__S,
         }
+
+    def extract_model_features(
+            self,
+    ) -> tuple:
+        """
+        Extract Heston model features using Approximate Bayesian Computing.
+        """
+        df = self.stock.history__
+        number_of_days = self.number_of_days
+        # Get the features of stochastic mean rate of return.
+        y = - (
+                (df['Risk free return']) * _NTD * 100
+        ).rolling(window=number_of_days).mean().diff(periods=-number_of_days)
+        x = (
+                (
+                    df['Risk free return'] - df['Risk free return'].mean()
+                ) * _NTD * 100
+        ).rolling(window=number_of_days).mean()
+        kde1 = self.__extract_kde(x=x, y=y, n=number_of_days,)
+        reg1 = self.__extract_regressor(x=x, y=y, n=number_of_days)
+        std1 = np.nanstd(x)
+        # Get the features of stochastic volatility.
+        y = (
+            (df['Risk free return']) * _NTD * 100
+        ).shift(periods=number_of_days).rolling(window=number_of_days).std() / (
+            (df['Risk free return']) * _NTD * 100
+        ).rolling(window=number_of_days).std()
+        x = (
+            df['Risk free return'] * _NTD * 100
+        ).rolling(window=number_of_days).std() / (
+            df['Risk free return'].std() * _NTD * 100
+        )
+        kde2 = self.__extract_kde(
+            x=vectorized_log(x),
+            y=vectorized_log(y),
+            n=2*number_of_days,
+        )
+        reg2 = self.__extract_regressor(
+            x=vectorized_log(x),
+            y=vectorized_log(y),
+            n=2*number_of_days
+        )
+        std2 = np.nanstd(vectorized_log(x))
+        return kde1, kde2, reg1, reg2, std1, std2
+
+    @staticmethod
+    def __extract_kde(
+        x: np.array,
+        y: np.array,
+        n: int,
+    ) -> gaussian_kde:
+        """Extract 2D kernel density function."""
+        values = np.vstack([x[n - 1: -n], y[n - 1: -n]])
+        return gaussian_kde(values)
+
+    @staticmethod
+    def __extract_regressor(
+        x: np.array,
+        y: np.array,
+        n: int,
+        intercept: bool = False,
+    ) -> object:
+        """Extract Huber regressor from x, y values."""
+        return Regressor(
+            fit_intercept=intercept,
+        ).fit(
+            X=np.array(x[n - 1: -n]).reshape(-1, 1),
+            y=np.array(y[n - 1: -n])
+        )
